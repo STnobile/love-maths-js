85baa3ad93a2e4d19921cca155b1f554fd13e36c
STnobile <stnobile0@gmail.com>
2022-12-23 17:00:25 +0000

    Display additiona question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,29 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             } 
         })
     }
+
+    runGame("addition")
 })
 
 
-function runGame() {
 
+function runGame (gameType) {
+
+
+   // Creates two random number between 1 and 25 
+   let num1 = Math.floor(Math.random() *25) + 1;
+    let num2 = Math.floor(Math.random() *25) + 1;
+
+    if (gameType === "addition") {
+        dispalyAdditionQuestion(num1, num2);
+    } else{
+        alert(`Unknown game type: ${gameType}`);
+        throw`Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -42,7 +56,10 @@ function incrementWrongAnswer() {
 }
 
 
-function dispalyAdditionQuestion() {
+function dispalyAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
 }
  
